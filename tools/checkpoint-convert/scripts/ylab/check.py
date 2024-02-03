4befbeab336436d5d2011b603b41fedaacba66df
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-03 15:07:32 +0900

    feat: check script

---
+++ tools/checkpoint-convert/scripts/ylab/check.py
@@ -0,0 +1,56 @@
+from transformers import MixtralForCausalLM
+import torch
+import argparse
+
+
+def compare_state_dicts(original_state_dict, converted_state_dict):
+    differences = {}
+
+    for key in original_state_dict.keys():
+        if key not in converted_state_dict:
+            differences[key] = ("Only in original state_dict", )
+            continue
+
+        if not torch.equal(original_state_dict[key], converted_state_dict[key]):
+            differences[key] = (original_state_dict[key].numpy(), converted_state_dict[key].numpy())  # テンソルの値をnumpy配列として保存
+
+    for key in converted_state_dict.keys():
+        if key not in original_state_dict:
+            differences[key] = ("Only in converted state_dict", )
+
+    return differences
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--base-hf-model-path", type=str, required=True)
+parser.add_argument("--converted-hf-model-path", type=str, required=True)
+args = parser.parse_args()
+
+# モデルをロード
+original_model = MixtralForCausalLM.from_pretrained(
+    args.base_hf_model_path,
+    device_map="cpu"
+)
+
+converted_model = MixtralForCausalLM.from_pretrained(
+    args.converted_hf_model_path,
+    device_map="cpu"
+)
+
+# state_dictを取得
+original_state_dict = original_model.state_dict()  # type: ignore
+converted_state_dict = converted_model.state_dict()  # type: ignore
+
+# state_dictの差分を比較
+diffs = compare_state_dicts(
+    original_state_dict=original_state_dict,
+    converted_state_dict=converted_state_dict
+)
+
+for key, values in diffs.items():
+    print(f"Key: {key}")
+    if len(values) == 2:
+        print(f"  original Value (Shape {values[0].shape}):\n{values[0]}")
+        print(f"  converted 2 Value (Shape {values[1].shape}):\n{values[1]}")
+    else:
+        print(f"  {values[0]}")
---
+++ tools/checkpoint-convert/scripts/ylab/check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#YBATCH -r dgx-a100_8
+#SBATCH --job-name=check
+#SBATCH --time=6:00:00
+#SBATCH --output outputs/check/%j.out
+#SBATCH --error errors/check/%j.err
+. /etc/profile.d/modules.sh
+module load cuda/11.8
+module load cudnn/cuda-11.x/8.9.0
+module load nccl/cuda-11.7/2.14.3
+module load openmpi/4.0.5
+
+# python virtualenv
+cd /home/kazuki/llama/Megatron-LM
+source .env/bin/activate
+
+python tools/checkpoint-convert/scripts/ylab/check.py \
+  --base-hf-model-path /home/kazuki/hf_checkpoints/Mixtral-8x7B-v0.1 \
+  --converted-hf-model-path /home/kazuki/converted_checkpoints/Mistral-8x7b/iter_0000005
